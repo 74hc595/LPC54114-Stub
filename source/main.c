d619e58b93ff8215581cbfdb5619cf0d45666bf7
Matt Sarnoff matt.sarnoff@gmail.com <matt.sarnoff@gmail.com>
2019-03-04 21:36:33 -0800

    rapidly toggle GPIO example
    On the LPCXpresso54114, this is the green LED. Should appear solid to
    the naked eye, but you should be able to see a square wave on pin A3 of
    the analog header with a scope.

--- source/main.c
+++ source/main.c
@@ -1,8 +1,23 @@
+#include "LPC54114_cm4.h"
+
+#define LED_PORT  1U
+#define LED_PIN   10U
 
 int main(void)
 {
+  /* enable IOCON and GPIO clocks */
+  SYSCON->AHBCLKCTRLSET[0] = (1U << 13) | (1U << (14+(LED_PORT)));
+  /* configure pin for GPIO */
+  IOCON->PIO[LED_PORT][LED_PIN] =
+    (0U << 0) | /* configured as GPIO */
+    (2U << 3) | /* pullup enabled */
+    (1U << 7);  /* digital mode */
+  /* set GPIO pin as an output */
+  GPIO->DIRSET[LED_PORT] = (1U << LED_PIN);
+
+  /* rapidly toggle GPIO */
   while (1) {
-    /* spin here */
+    GPIO->NOT[LED_PORT] = (1U << LED_PIN);
   }
   return 0;
 }
